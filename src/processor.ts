47c55060caaf61fca9881d325a875f9d4a551181
Egor Koleda <radioegor146@ctf.by>
2026-04-28 03:29:40 +0300

    wut?

--- src/processor.ts
+++ src/processor.ts
@@ -90,6 +90,7 @@ class OpenAITransformStream extends TransformStream {
   constructor() {
     super({
       transform(chunk, controller) {
+        console.info(chunk)
         const part = chunk.choices[0]?.delta?.content
         if (!part) {
           controller.terminate()
