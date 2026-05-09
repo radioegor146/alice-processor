774b59644e96dda64c0798f51c7621c20016f5a9
Egor Koleda <radioegor146@ctf.by>
2026-05-10 02:33:30 +0300

    try

--- src/processor.ts
+++ src/processor.ts
@@ -175,15 +175,27 @@ export class Processor {
           webSocket.send(JSON.stringify({
             data: {
               directives: response.directives,
-              finished: true,
-              requireMoreInput: response.requireMoreInput,
+              finished: false,
               sessionId: response.sessionId,
               text: response.text
             },
             type: 'partialResponse'
           }), () => {
-            this.logger.info('WebSocket close')
-            webSocket.close()
+            this.logger.info('deferring for test')
+            setTimeout(() => {
+              webSocket.send(JSON.stringify({
+                data: {
+                  directives: [],
+                  finished: true,
+                  requireMoreInput: response.requireMoreInput,
+                  sessionId: response.sessionId,
+                  text: 'жопа'
+                },
+                type: 'partialResponse'
+              }))
+              this.logger.info('WebSocket close')
+              webSocket.close()
+            }, 10_000)
           })
           break
         }
